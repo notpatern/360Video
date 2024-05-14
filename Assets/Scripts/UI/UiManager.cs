68f0d918e6118cafdc70064904b4403fe583fb08
Sasha <132772266+notpatern@users.noreply.github.com>
2024-05-14 17:29:04 +0200

    feat: serialize fields

--- Assets/Scripts/UI/UiManager.cs
+++ Assets/Scripts/UI/UiManager.cs
@@ -1,8 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UiManager : MonoBehaviour
 {
-    
+    [SerializeField] Button pauseButton;
+    [SerializeField] Dropdown playBackSpeedSelector;
+    [SerializeField] Slider videoTimeLine;
+
+    // subs to events which will be referenced in the manager etc. Should be pretty easy
 }
